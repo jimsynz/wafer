c87c4a9f322e462e014a4fe29e4317e33a95040d
James Harton <james@harton.nz>
2020-12-28 18:16:12 +1300

    chore(deps): Use the newly release 2.3 version of `git_ops`.

--- mix.exs
+++ mix.exs
@@ -50,12 +50,7 @@ defmodule Wafer.MixProject do
       {:earmark, ">= 0.0.0", only: ~w[dev test]a},
       {:elixir_ale, "~> 1.2", optional: true},
       {:ex_doc, ">= 0.0.0", only: ~w[dev test]a},
-      # {:git_ops, "~> 2.2", only: ~w[dev test]a, runtime: false},
-      {:git_ops,
-       git: "https://github.com/jimsynz/git_ops",
-       branch: "add-dotenv-format",
-       only: ~w[dev test]a,
-       runtime: false},
+      {:git_ops, "~> 2.3", only: ~w[dev test]a, runtime: false},
       {:mimic, "~> 1.1", only: :test}
     ]
   end
