db68e3f027a05bb306950c83d4be9be218a972d2
James Harton <james@harton.nz>
2023-11-24 14:57:50 +1300

    fix: compiler warning in `Wafer.Driver.ElixirALE.I2C.Wrapper.write_read_device/4`.

--- lib/wafer/drivers/elixir_ale/i2c/wrapper.ex
+++ lib/wafer/drivers/elixir_ale/i2c/wrapper.ex
@@ -35,5 +35,5 @@ defmodule Wafer.Driver.ElixirALE.I2C.Wrapper do
   defdelegate write_read(pid, write_data, read_count), to: ElixirALE.I2C
 
   @compile {:no_warn_undefined, ElixirALE.I2C}
-  defdelegate write_read_device(pid, address, write_data, read_count), to: ElixirALE
+  defdelegate write_read_device(pid, address, write_data, read_count), to: ElixirALE.I2C
 end
