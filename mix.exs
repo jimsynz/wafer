41081bc523f4ad8193bc32dafbe118a2a7db89fc
James Harton <james@harton.nz>
2023-11-24 15:40:26 +1300

    chore: add `mix audit`.

--- mix.exs
+++ mix.exs
@@ -62,7 +62,8 @@ defmodule Wafer.MixProject do
       {:ex_check, "~> 0.15", devtest},
       {:ex_doc, ">= 0.0.0", devtest},
       {:git_ops, "~> 2.4", devtest},
-      {:mimic, "~> 1.5", only: :test}
+      {:mimic, "~> 1.5", only: :test},
+      {:mix_audit, "~> 2.1", devtest}
     ]
   end
 end
